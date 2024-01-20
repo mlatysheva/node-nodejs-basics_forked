c7f6760287a5004885a5c83d72520d06f83a0516
mlatysheva <mlatysheva@gmail.com>
2024-01-20 21:55:35 +0100

    feat: added esm and calcHash functions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { getResolvedPath } from '../utils/getResolvedPath.js';
+import { CONSOLE_COLORS } from '../constants/consoleColors.js';
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const fileToHash = getResolvedPath(__filename, 'files', 'fileToCalculateHashFor.txt');
+    const content = await readFile(fileToHash);
+    const hash = createHash('sha256').update(content).digest('hex');
+    console.log(CONSOLE_COLORS.green, `The hash for the file "fileToCalculateHashFor.txt" is ${hash}`);
+  } catch(err) {
+    console.error(err);
+  }  
+}
 
 await calculateHash();
