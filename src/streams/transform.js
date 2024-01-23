43ec1af9c7ac88cd3324d987caff3c54b0c20bab
mlatysheva <mlatysheva@gmail.com>
2024-01-23 19:55:19 +0100

    feat: add streams functions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+import { throwErrorMessage } from '../utils/throwErrorMessage.js';
+
+export const transform = async () => {
+  try {
+    process.stdout.write('Enter text or type "exit" to exit the program:\n');  
+
+    const reversedStream = new Transform();
+
+    reversedStream._transform = (chunk, _, callback) => {  
+      const entry = chunk.toString();
+      if (entry.trim() === 'exit') {
+        process.exit();
+      }
+      const reversedChunk = entry.split('').reverse().join('') + '\n';
+      callback(null, reversedChunk);
+    }
+    await pipeline(process.stdin, reversedStream, process.stdout);
+  } catch(err) {
+    throwErrorMessage(err);
+  }
 };
 
 await transform();
