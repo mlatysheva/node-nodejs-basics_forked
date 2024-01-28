981e6d8b1a42d5bf87eb095b53a7429592a7db21
mlatysheva <mlatysheva@gmail.com>
2024-01-28 22:02:43 +0100

    feat: added cp functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import { spawn } from 'node:child_process';
+import { getResolvedPath } from '../utils/getResolvedPath.js';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  try {
+    const _filename = fileURLToPath(import.meta.url);
+    const scriptToExecute = getResolvedPath(_filename, 'files', 'script.js');
+    const child = spawn('node', [scriptToExecute, ...args]);
+
+    process.stdin.on('data',(msg) => {
+      if (msg.toString().trim() === 'CLOSE'.toLowerCase()) {
+        process.exit(0);
+      };
+      child.stdin.write(msg); 
+    });
+    
+    child.stdout.on('data',(msg) => {
+      process.stdout.write(msg);
+    });
+
+    child.stdout.on('close', (code) => {
+      process.exit(code);
+    });
+  } catch(err) {
+    console.error(err);
+  }  
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['rss', 'node.js', 'basics']);
